5d4b8a72e625bf50681a4f53a4faf2ba46577d58
Anonymous <shanto@nextgenitltd.com>
2025-09-23 12:50:14 +0600

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use App\Http\Controllers\AdminController;
+use App\Models\User;
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
+
+});
